4d392a3ff26f930ce445da9d4d54ade40c30f537
Ali Fatahi <a.fatahi1992@gmail.com>
2018-07-17 20:17:18 +0430

    Pass Our Model To Admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Since we are in same Directory we pass .models instead of posts.models
+from .models import Post
+
+admin.site.register(Post)
+
