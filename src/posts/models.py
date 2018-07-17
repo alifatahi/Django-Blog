c5e0a543968625ff69ba07d36d99c2772477ff2a
Ali Fatahi <a.fatahi1992@gmail.com>
2018-07-17 19:59:38 +0430

    Create New App: Post Create Migration Install it

---
+++ src/posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    # we make auto_now True because its save now over and over not created(add)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    # we make it false because its not save now its created
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
